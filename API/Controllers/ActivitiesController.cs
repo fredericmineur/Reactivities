e38c797bf4165e22b7860a745cfb9c6ffaf74346
Frederic Mineur <fredericmineur@gmail.com>
2021-06-16 13:31:50 +0100

    add an 'edit' handler with title edit functionality

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -28,10 +28,17 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreatActivity(Activity activity)
+        public async Task<IActionResult> CreateActivity(Activity activity)
         {
             return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
         }
+
+[HttpPut("{id}")]
+public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+{
+    activity.Id = id;
+    return Ok(await Mediator.Send(new Edit.Command{Activity=activity}));
+}
 
 
     }
