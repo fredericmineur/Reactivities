b8f98c385ee51f9a7b02f303c9080e34f2dc2875
Frederic Mineur <fredericmineur@gmail.com>
2021-06-16 14:37:52 +0100

    add Automapper to edit all properties

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
@@ -16,17 +17,19 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
-//Here we update our activity with the title from our request body,
-// or with the old the title is no new titles is in the request
-                activity.Title = request.Activity.Title ?? activity.Title;
+
+                _mapper.Map(request.Activity, activity);
+
                 await _context.SaveChangesAsync();
 
                 return Unit.Value;
