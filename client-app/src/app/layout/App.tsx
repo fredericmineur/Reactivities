724532868a4dd702bff36ad23b71b09a30363533
Frederic Mineur <fredericmineur@gmail.com>
2021-06-17 23:19:13 +0100

    add style to NavBar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, Header, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 
@@ -17,14 +17,17 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
+    <>
       <NavBar />
-      <List>
-        {activities.map((activity) =>
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        )}
-      </List>
-    </div>
+      <Container style={{marginTop: "7em"}}>
+        <List>
+          {activities.map((activity) =>
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          )}
+        </List>
+      </Container>
+
+    </>
   );
 }
 
