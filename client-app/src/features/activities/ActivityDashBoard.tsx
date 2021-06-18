10aa54ca9d0ca618dcb01593f3dc712fddc2d9b6
Frederic Mineur <fredericmineur@gmail.com>
2021-06-18 10:24:39 +0100

    add Form component

--- client-app/src/features/activities/ActivityDashBoard.tsx
+++ client-app/src/features/activities/ActivityDashBoard.tsx
@@ -3,6 +3,7 @@ import { Grid, List } from "semantic-ui-react";
 import { Activity } from "../../app/models/activity";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "./details/ActivityDetails";
+import ActivityForm from "./form/ActivityForm";
 
 interface Props {
     activities: Activity[]
@@ -14,10 +15,12 @@ export default function ActivityDashBoard({ activities }: Props) {
             <Grid.Column width='10'>
                 <ActivityList activities={activities} />
             </Grid.Column>
-<Grid.Column width='6'>
-    {activities[0] && 
-    <ActivityDetails  activity={activities[0]}/> }
-</Grid.Column>
+            <Grid.Column width='6'>
+                {activities[0] &&
+                <ActivityDetails activity={activities[0]} />}
+                <ActivityForm />
+
+            </Grid.Column>
         </Grid>
 
     );
