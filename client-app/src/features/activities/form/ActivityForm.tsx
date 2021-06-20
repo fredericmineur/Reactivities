9e263914144955c20723dda7204f8dcc6bc59e72
Frederic Mineur <fredericmineur@gmail.com>
2021-06-20 16:06:48 +0100

    remove unused imports

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useState } from "react";
-import { Button, Form, Segment, SegmentProps } from "semantic-ui-react";
+import { Button, Form, Segment} from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 
 
