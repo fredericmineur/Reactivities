d66a6baf333dcbe3b46978b7321514ce239329ea
Frederic Mineur <fredericmineur@gmail.com>
2021-06-24 01:25:03 +0100

    add loading activities to MobX store

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Container } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashBoard from '../../features/activities/ActivityDashBoard';
@@ -18,20 +18,24 @@ function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
   const [submitting, setSubmitting] = useState(false);
 
+  // useEffect(() => {
+  //   agent.Activities.list()
+  //     .then(response => {
+  //       let activities: Activity[] = [];
+  //       response.forEach(activity => {
+  //         activity.date = activity.date.split('T')[0];
+  //         activities.push(activity)
+  //       })
+  //       setActivities(activities);
+  //       setLoading(false);
+  //     })
+  // }, [])
+
   useEffect(() => {
-    agent.Activities.list()
-      .then(response => {
-        let activities: Activity[] = [];
-        response.forEach(activity => {
-          activity.date = activity.date.split('T')[0];
-          activities.push(activity)
-        })
-        setActivities(activities);
-        setLoading(false);
-      })
+    activityStore.loadActivities();
   }, [])
 
 
@@ -81,16 +85,15 @@ function App() {
     })
   }
 
-  if (loading) return <LoadingComponent content='Loading app' />
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
 
   return (
     <>
       <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: "7em" }}>
-<h2>{activityStore.title}</h2>
-<Button  content='Add exclamation' positive onClick={activityStore.setTitle} />
+
         <ActivityDashBoard
-          activities={activities}
+          activities={activityStore.activities}
           selectedActivity={selectedActivity}
           selectActivity={handleSelectActivity}
           cancelSelectActivity={handleCancelSelectActivity}
