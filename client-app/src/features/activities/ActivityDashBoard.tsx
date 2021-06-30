8510c9dffd97b95df6a3336cab6bf18841b92c60
Frederic Mineur <fredericmineur@gmail.com>
2021-06-30 17:18:28 +0100

    using route parameters for activity details

--- client-app/src/features/activities/ActivityDashBoard.tsx
+++ client-app/src/features/activities/ActivityDashBoard.tsx
@@ -4,15 +4,13 @@ import { Grid } from "semantic-ui-react";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useStore } from "../../app/stores/store";
 import ActivityList from "./ActivityList";
-import ActivityDetails from "./details/ActivityDetails";
-import ActivityForm from "./form/ActivityForm";
 
 
 
 export default observer(function ActivityDashBoard() {
 
     const { activityStore } = useStore();
-    const { selectedActivity, editMode } = activityStore;
+
 
 
     useEffect(() => {
@@ -28,10 +26,7 @@ export default observer(function ActivityDashBoard() {
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
-                {selectedActivity && !editMode &&
-                    <ActivityDetails />}
-                {editMode &&
-                    <ActivityForm />}
+                <h2>Activity Filter</h2>
 
             </Grid.Column>
         </Grid>
