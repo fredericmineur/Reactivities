f20d88d11466438a1349969ce71f2d09f1b1bc23
Frederic Mineur <fredericmineur@gmail.com>
2021-07-04 20:27:56 +0100

    add modify axios interceptor for errors + toaster messages

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,8 +1,9 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
 import { Activity } from "../models/activity";
 
 const sleep = (delay: number) => {
-    return new Promise((resolve)=> {
+    return new Promise((resolve) => {
         setTimeout(resolve, delay);
     });
 }
@@ -11,30 +12,44 @@ const sleep = (delay: number) => {
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
 axios.interceptors.response.use(async response => {
-    try {
-        await sleep(1000);
-        return response;
-    } catch (error) {
-        console.log(error);
-        return await Promise.reject(error);
+    await sleep(1000);
+    return response;
+
+},(error:AxiosError) => {
+    const {data, status} = error.response!;
+    switch (status) {
+        case 400:
+            toast.error('bad request');
+            break;
+        case 401:
+            toast.error('unauthorised');
+            break;
+        case 404:
+            toast.error('not found');
+            break;
+        case 500:
+            toast.error('server error');
+            break;
+
     }
+    return Promise.reject(error);
 })
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
     get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-    post: <T>(url: string, body:{}) => axios.post<T>(url, body).then(responseBody),
-    put: <T>(url: string, body:{}) => axios.put<T>(url, body).then(responseBody),
+    post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
     del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 }
 
 const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
-    details : (id:string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity)=>requests.post<void>('/activities', activity), 
+    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+    create: (activity: Activity) => requests.post<void>('/activities', activity),
     update: (activity: Activity) => requests.put<void>(`/activities/${activity.id}`, activity),
-    delete: (id:string) => requests.del<void>(`/activities/${id}`)
+    delete: (id: string) => requests.del<void>(`/activities/${id}`)
 
 }
 
