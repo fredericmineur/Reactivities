188c40e8e7cb182c1c740ed46460ea4b479fef54
Frederic Mineur <fredericmineur@gmail.com>
2021-07-07 00:35:58 +0100

    handling 400 error to return not found Component for invalid GUID

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { history } from "../..";
 import { Activity } from "../models/activity";
+import { store } from "../stores/store";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -16,20 +17,24 @@ axios.interceptors.response.use(async response => {
     await sleep(1000);
     return response;
 
-},(error:AxiosError) => {
-    const {data, status} = error.response!;
+}, (error: AxiosError) => {
+    const { data, status, config } = error.response!;
     switch (status) {
         case 400:
-            if(data.errors){
+            if(typeof data === 'string') {
+                toast.error(data);
+            }
+            if(config.method ==='get' && data.errors.hasOwnProperty('id')){
+                history.push('/not-found')
+            }
+            if (data.errors) {
                 const modalStateErrors = [];
-                for (const key in data.errors){
-                    if(data.errors[key]){
+                for (const key in data.errors) {
+                    if (data.errors[key]) {
                         modalStateErrors.push(data.errors[key])
                     }
                 }
                 throw modalStateErrors.flat();
-            } else {
-                toast.error(data);
             }
             break;
         case 401:
@@ -39,7 +44,8 @@ axios.interceptors.response.use(async response => {
             history.push('/not-found')
             break;
         case 500:
-            toast.error('server error');
+            store.commonStore.setServerError(data);
+            history.push('/server-error');
             break;
 
     }
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -10,6 +10,7 @@ import ActivityDashBoard from '../../features/activities/dashboard/ActivityDashB
 import TestErrors from '../../features/errors/TestError';
 import { ToastContainer } from 'react-toastify';
 import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 
 
@@ -32,6 +33,7 @@ function App() {
     <Route path='/activities/:id' component={ActivityDetails} />
     <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
     <Route path='/errors' component={TestErrors} />
+    <Route path='/server-error' component={ServerError} />
     <Route component={NotFound} />
   </Switch>
   
